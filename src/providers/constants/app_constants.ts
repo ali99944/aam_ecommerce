027f5ce682f8dc0b9354410b17e13c9a571c1d84
ali99944 <alitarek99944@gmail.com>
2025-04-16 18:32:52 +0200

    update api link

--- src/providers/constants/app_constants.ts
+++ src/providers/constants/app_constants.ts
@@ -1,6 +1,10 @@
 const AppConstants = Object.freeze({
-    api_url: 'http://localhost/api',
-    storage_url: 'http://localhost/storage',
+    // api_url: 'http://localhost/api',
+    // storage_url: 'http://localhost/storage',
+
+
+    api_url: 'https://masoud-api.techconnects.fr/api',
+    storage_url: 'https://masoud-api.techconnects.fr/storage',
 })
 
 export default AppConstants
